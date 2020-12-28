064bd9c576ac95b0562763f31afb5176956cf10f
xirothehero <ali.kedwaii@utexas.edu>
2020-12-28 02:09:53 -0600

    Game logic working

---
+++ Assets/Xiro/Scripts/Food.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Food")]
+public class Food : ScriptableObject
+{
+    public string type;
+    public Sprite sprite;
+}
